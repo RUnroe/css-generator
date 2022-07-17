5a7273f4b10ef4cdf7b4a551e9eb7ac251163edc
RUnroe <ryanunroe@gmail.com>
2022-07-16 21:54:44 -0600

    Create header and remove pro-sidebar

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>CSS Generator</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+  }
+  
+  export default Header;
+  
