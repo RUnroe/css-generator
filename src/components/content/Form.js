0de0af88628be07e771da440e2a2c16ed8706b8a
RUnroe <ryanunroe@gmail.com>
2022-07-20 09:31:10 -0600

    Add radio buttons to style sheet

--- src/components/content/Form.js
+++ src/components/content/Form.js
@@ -9,6 +9,19 @@ const Form = () => {
     <input id="demo-input" type="text" />
 </div>
     `);
+    const [inputTypeCode, setInputTypeCode] = useState(`
+<div className="input-group">
+    <label htmlFor="demo-textarea">Textarea Label:</label>
+    <textarea id="demo-textarea" type="text" textarea/>
+    <input type="checkbox" />
+    <input type="radio" />
+    <select>
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+    </select>
+</div>
+    `);
     return (  
         <div id='form' className='form-section section'>
             <a href='#form'><h2>Form</h2></a>
@@ -18,6 +31,7 @@ const Form = () => {
                 <h3>Input Groups</h3>
                 <p>A wrapper for a form input and label. This wrapper adds styles to its children inputs, textareas, and labels.</p>
                 <div className="demo-container">
+                    <h4>Basic input-group</h4>
                     <div className="demo-display">
                         <div className="input-group">
                             <label htmlFor="demo-input">Input Label:</label>
@@ -30,6 +44,28 @@ const Form = () => {
                     </SyntaxHighlighter>
 
                 </div>
+                <div className="demo-container">
+                    <h4>Types of inputs</h4>
+                    <div className="demo-display">
+                        <div className="input-group">
+                            <label htmlFor="demo-textarea">Textarea Label:</label>
+                            <textarea id="demo-textarea" type="text" textarea/>
+                            <input type="checkbox" />
+                            <input type="radio" />
+                            <select>
+                                <option>Option 1</option>
+                                <option>Option 2</option>
+                                <option>Option 3</option>
+                            </select>
+                        </div>
+                    </div>
+                
+                    <SyntaxHighlighter language="html" style={vs2015}>
+                        {inputTypeCode}
+                    </SyntaxHighlighter>
+
+                </div>
+                
             </div>
 
         </div>
